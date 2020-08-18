e4e9ee72dd2aa0d89883c12841dedf092d09eb9d
jaxme <jaxme@DESKTOP-9CUMEUJ>
2020-08-17 19:58:34 -0700

    good day

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,22 +1,27 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
-	//2. create an array of 5 robots.
-
-	//3. use a for loop to initialize the robots.
-
+	public static void main(String[] args) {
+		//2. create an array of 5 robots.
+		Robot[] joe = new Robot[5];
+		//3. use a for loop to initialize the robots.
+		for (int i = 0; i < joe.length; i++) {
+			joe[i] = new Robot();
+		}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
+		
+		//5. use another for loop to iterate through the array and make each robot move 
+		//   a random amount less than 50.
 
-	//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
+		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+		//7. declare that robot the winner and throw it a party!
 
-	//7. declare that robot the winner and throw it a party!
+		//8. try different races with different amounts of robots.
 
-	//8. try different races with different amounts of robots.
-
-	//9. make the robots race around a circular track.
+		//9. make the robots race around a circular track.
+	}
 }
